8272c294c8583b99ea196841a558f508a8cfbbe5
Oleg72pc <oleg72pc@mail.ru>
2022-09-14 01:26:58 +0300

    deploy-v1

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/img/logo.svg';
